308f0cd98147cfba8922f111547a5ab6865085d5
student <s1071927@gm.pu.edu.tw>
2020-05-15 14:22:31 +0800

    第二次commit (修改文字元件)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView txv = (TextView) findViewById(R.id.txv);
+        txv.setText("Hello tcyang");
     }
 }
